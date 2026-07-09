45cb64f9a95beb2c7af268504e287023b0c4fc0e
Newf <ian@kellys.me>
2026-07-09 08:46:21 -0700

    cleaner

--- plugin.program.cleaner/uservar.py
+++ plugin.program.cleaner/uservar.py
@@ -5,7 +5,7 @@ buildfile = 'https://www.dropbox.com/scl/fi/vrdxxv76qvdce1kvklvpt/builds.xml?rlk
 videos_url = 'https://youtu.be/DO_W22vggGE'
 
 '''#####-----Notification File-----#####'''
-notify_url  = 'https://www.dropbox.com/scl/fi/mu69l2myt99u79yd4cpwe/build-notify.txt?rlkey=b7kydu5o4wt0e2klg0fto4sqq&st=24bxjlpe&dl=1'
+notify_url  = ''
 
 '''#####-----Changelog Directory-----#####'''
 changelog_dir  = 'https://github.com/%s/%s/raw/master/packages/%s'
